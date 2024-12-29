045f8dd227937c3e301a0905f26f5f48ef219f4e
kishore1115 <161201823+kishore1115@users.noreply.github.com>
2024-12-29 23:43:46 +0530

    Update index.js

--- Calender-app/src/index.js
+++ Calender-app/src/index.js
@@ -16,24 +16,3 @@ root.render(
         <App />
     </Provider>
 );
-
-App.js:
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-import AdminModule from './components/AdminModule';
-import UserModule from './components/UserModule';
-import process from 'process';
-
-const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/admin" element={<AdminModule />} />
-                <Route path="/user" element={<UserModule />} />
-                <Route path="/" element={<UserModule />} />
-            </Routes>
-        </Router>
-    );
-};
-
-export default App;
