a6f8832c5665032380bc506ac6591333c4486e96
kishore1115 <161201823+kishore1115@users.noreply.github.com>
2024-12-29 23:34:39 +0530

    Update companyrow.js

--- Calender-app/src/components/usermodule.js/companyrow.js
+++ Calender-app/src/components/usermodule.js/companyrow.js
@@ -1,1 +1,29 @@
 
+// src/components/UserModule/CompanyRow.js
+import React from 'react';
+import { formatDistanceToNow } from 'date-fns';
+
+const CompanyRow = ({ company }) => {
+  const { name, communications, nextCommunication } = company;
+  
+  const lastFiveCommunications = communications.slice(-5).map((comm, index) => (
+    <div key={index}>
+      {comm.type} - {comm.date}
+    </div>
+  ));
+
+  const timeUntilNextCommunication = formatDistanceToNow(new Date(nextCommunication), { addSuffix: true });
+
+  return (
+    <tr className="company-row">
+      <td>{name}</td>
+      <td>{lastFiveCommunications}</td>
+      <td>{timeUntilNextCommunication}</td>
+      <td>
+        <button className="btn-action">View Details</button>
+      </td>
+    </tr>
+  );
+};
+
+export default CompanyRow;
