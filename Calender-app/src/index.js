0a1181054398dfd655060af5a5d7481ae185ec7d
kishore1115 <161201823+kishore1115@users.noreply.github.com>
2024-12-29 23:43:05 +0530

    Update index.js

--- Calender-app/src/index.js
+++ Calender-app/src/index.js
@@ -5,12 +5,35 @@ import { createStore } from 'redux';
 import App from './App';
 import rootReducer from './redux/reducers';
 import './styles/index.css';
+import { createRoot } from 'react-dom/client';
+import process from 'process';
 
 const store = createStore(rootReducer);
-
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
+
+App.js:
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import AdminModule from './components/AdminModule';
+import UserModule from './components/UserModule';
+import process from 'process';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/admin" element={<AdminModule />} />
+                <Route path="/user" element={<UserModule />} />
+                <Route path="/" element={<UserModule />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
